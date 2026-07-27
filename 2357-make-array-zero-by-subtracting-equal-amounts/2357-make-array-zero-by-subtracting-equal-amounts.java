5054be745657175595ae23017967869c204d0043
Ramgupta-1905 <ramgupta1906@gmail.com>
2026-07-27 23:33:46 +0530

    Time: 1 ms (93.6%), Space: 43.1 MB (38.72%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,20 +1,10 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for(int i =0;i<nums.length;i++){
-            if(nums[i]!=0)
-            queue.add(nums[i]);
-        }
-        int count =0;
-        int sum =0;
-        while(!queue.isEmpty()){
-            int x = queue.poll();
-            int rest = x- sum;
-            if(rest != 0){
-                count++;
-            }
-            sum = sum +rest;
-        }
-        return count;
+        HashSet<Integer> set = new HashSet<>();
+        for(int x: nums)
+            if(x !=0)
+                set.add(x);
+
+        return set.size();
     }
 }
