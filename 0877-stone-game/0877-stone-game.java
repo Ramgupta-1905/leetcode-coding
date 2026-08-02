7e6a30c7e478a5978838905979022def83609747
Ramgupta-1905 <ramgupta1906@gmail.com>
2026-08-02 09:11:03 +0530

    Time: 0 ms (100%), Space: 43.2 MB (48.3%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        Integer[][] memo = new Integer[n][n];
-       return solve(0,n-1,piles,memo) >0;
-    
-    }
-    private int solve(int left,int right,int[] nums,Integer[][] memo ){
-         if(left == right)
-            return nums[left];
-        if(memo[left][right] !=null) return memo[left][right];
-        int leftSum = nums[left] - solve(left+1,right,nums,memo);
-        int rightSum = nums[right] - solve(left,right-1,nums,memo);
-
-        return memo[left][right]=Math.max(leftSum,rightSum);
+        return true;
     }
 }
