9146433203d7986064a2c1e03491a608e02ce60e
Ramgupta-1905 <ramgupta1906@gmail.com>
2026-08-08 23:42:44 +0530

    Time: 2 ms (10.47%), Space: 43.8 MB (41.59%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int myAtoi(String s) {
+        int sign =1;
+        s = s.trim();
+        if(s.length() == 0) return 0;
+        long res = 0;
+        int start =0;
+        if(s.charAt(0) == '-'){
+                 sign =-1;
+                 start = 1;
+        }
+        else if(s.charAt(0) == '+')
+            start =1;
+        else if( Character.isDigit(s.charAt(0)))
+            start =0;
+        else
+            return 0;
+
+        for(int i = start;i<s.length();i++){
+           if(Character.isDigit(s.charAt(i))){
+                res = res*10 + (s.charAt(i) -'0');
+                 if(res * sign > Integer.MAX_VALUE)
+                    return Integer.MAX_VALUE;
+                else if(res* sign < Integer.MIN_VALUE)
+                    return Integer.MIN_VALUE;
+           }
+           else
+                break;
+        }
+        res = res*sign;
+       
+        return (int)res;
+            
+    }
+}
