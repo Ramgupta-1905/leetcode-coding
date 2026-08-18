dc6be7d67d336ede23c31e9e048f468700aa46c8
Ramgupta-1905 <ramgupta1906@gmail.com>
2026-08-18 11:32:35 +0530

    Time: 5 ms (54.8%), Space: 48.3 MB (77.95%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -1,28 +1,47 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode doubleIt(ListNode head) {
-        int finalcarr = multiply(head);
-        if(finalcarr != 0){
-        ListNode temp = new ListNode(finalcarr,head);
-        return temp;
-        }
-        return head;
-    }
-    public int multiply(ListNode curr){
-        if(curr == null)
-            return 0;
-        int carry = multiply(curr.next);
-        int x = curr.val*2 +carry;
-        curr.val = x%10;
-        return x/10;
-    }
+/** 
+ * Definition for singly-linked list. 
+ * public class ListNode { 
+ *     int val; 
+ *     ListNode next; 
+ *     ListNode() {} 
+ *     ListNode(int val) { this.val = val; } 
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; } 
+ * } 
+ */ 
+class Solution { 
+    public ListNode doubleIt(ListNode head) { 
+         
+        head = reverse(head); 
+        ListNode curr = head; 
+        int carry =0; 
+        ListNode prev = null; 
+ 
+        while(curr != null){ 
+            int x = curr.val *2 +carry; 
+            curr.val = x %10; 
+            carry = x/10; 
+            prev = curr; 
+            curr = curr.next; 
+        } 
+ 
+        if(carry != 0){ 
+            ListNode carr = new ListNode(carry); 
+            prev.next = carr; 
+            carr.next = null; 
+        } 
+ 
+        return reverse(head); 
+    } 
+    public ListNode reverse(ListNode head){ 
+         
+        ListNode prev = null; 
+        ListNode curr = head; 
+        while(curr != null){ 
+            ListNode next = curr.next; 
+            curr.next = prev; 
+            prev = curr; 
+            curr = next; 
+        } 
+        return prev; 
+    } 
 }
