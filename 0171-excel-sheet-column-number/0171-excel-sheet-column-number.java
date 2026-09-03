75d67f101e56f281baccdb53fc9249004b57a9a5
Ramgupta-1905 <ramgupta1906@gmail.com>
2026-09-04 00:54:31 +0530

    Time: 1 ms (90.46%), Space: 43.8 MB (17.84%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,10 +1,5 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        int c = 65;
-        for(int i =0;i<26;i++){
-            map.put((char)c++,i+1);
-        }
         int ans =0;
         int size = columnTitle.length();
         for(int i = 0;i<size;i++){
